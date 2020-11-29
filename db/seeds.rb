0a843d06c3bce69849771183ef3fa5ad5c764449
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2020-11-30 01:13:14 +0300

    adding seeds to db and set rpoduction to pg

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Client.create(client_id: 1001 , name: "Thuriya Medical Center")
-# Client.create(client_id: 1002 , name: "Banoon Medical Center")
-# Client.create(client_id: 1003 , name: "Soliman AL-Habib")
-# Client.create(client_id: 1004 , name: "Al-Falah Hospital")
-# Client.create(client_id: 1005, name: "Obiad Hospital")
-Client.create(client_id:1000000 , name: "Soliman Al-Habib Hospital Sweedy")
+Client.create(client_id: 1001 , name: "Thuriya Medical Center")
+Client.create(client_id: 1002 , name: "Banoon Medical Center")
+Client.create(client_id: 1003 , name: "Soliman AL-Habib")
+Client.create(client_id: 1004 , name: "Al-Falah Hospital")
+Client.create(client_id: 1005, name: "Obiad Hospital")
+Client.create(client_id:1000000 , name: "Soliman Al-Habib Hospital")
+
+Employer.create(username: "IBSA_Admin" , email: "ibsa_admin@ibsagulf.com" , password: "12345" , password_confirmation: "12345" , role: "GM")
