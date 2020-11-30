2f098268a8695ff36631bbdc5e3e120ffbf42435
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2020-11-30 23:19:48 +0300

    g

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Client.create(client_id: 1001 , name: "Thuriya Medical Center")
-Client.create(client_id: 1002 , name: "Banoon Medical Center")
-Client.create(client_id: 1003 , name: "Soliman AL-Habib")
-Client.create(client_id: 1004 , name: "Al-Falah Hospital")
-Client.create(client_id: 1005, name: "Obiad Hospital")
-
-Employer.create(username: "IBSA Admin" , email: "ibsa_admin@ibsagulf.com" , password: "12345" , password_confirmation: "12345" , role: "GM")
+GeneralManager.create(employer_id: 1)
